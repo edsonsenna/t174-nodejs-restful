c795ec1911ceab1f6415cc2a57943c8292977888
aluno pos <alunopos@local.inatel.br>
2019-11-23 14:54:35 -0200

    Rest Routes

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -14,10 +14,49 @@ router.get('/', (request, response) => {
 
 });
 
+router.get('/:id', (request, response) => {
+
+  const data = {
+    message: `Handling ${request.protocol} ${request.method} by ID ${request.params.id} requests for tasks!`
+  }
+
+  response
+    .status(HttpStatus.OK)
+    .json(data);
+  
+})
+
 router.post('/', (request, response) => {
 
   const data = {
     message: 'Handling POST resquests for tasks!'
+  }
+
+  response
+    .status(HttpStatus.OK)
+    .json(data);
+
+});
+
+router.patch('/:id', (request, response) => {
+
+  const data = {
+    id: request.params.id,
+    data: request.body,
+    message: 'Handling PATCH resquests for tasks!'
+  }
+
+  response
+    .status(HttpStatus.OK)
+    .json(data);
+
+});
+
+router.delete('/:id', (request, response) => {
+
+  const data = {
+    id: request.params.id,
+    message: 'Handling DELETE resquests for tasks!'
   }
 
   response
