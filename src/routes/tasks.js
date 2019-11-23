7c8ebb9035dc9ddcdbefc9a13375857e6270410c
aluno pos <alunopos@local.inatel.br>
2019-11-23 14:28:52 -0200

    Add Http Get

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const HttpStatus = require('http-status-codes');
 
+router.get('/', (request, response) => {
+
+  const data = {
+    message: `Handling ${request.protocol} ${request.method} requests for tasks!`
+  }
+
+  response
+    .status(HttpStatus.OK)
+    .json(data);
+
+});
+
 router.post('/', (request, response) => {
 
   const data = {
